8ec3be22b4b5c4dd4278a3528c653cbc84aa93e0
zvdas <rodriguezzude@gmail.com>
2022-11-03 11:53:51 +0530

    track model created & seeder executed successfully

--- backend_nodejs_v1/models/Track.js
+++ backend_nodejs_v1/models/Track.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const TrackSchema = new mongoose.Schema({
     track_name: {
         type: String,
+        trim: true,
         required: [true, 'Please add a name for the Track']
     },
+    slug: String,
     featuring: String,
     duration: String,
     track_file: {
@@ -16,7 +19,19 @@ const TrackSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now()
+    },
+    album: {
+        type: mongoose.Schema.ObjectId,
+        // reference to model for relationship
+        ref: 'Album',
+        required: true
     }
+});
+
+// create track slug from the name
+TrackSchema.pre('save', function(next) {
+    this.slug = slugify(this.track_name, { lower: true });
+    next();
 });
 
 module.exports = mongoose.model('Track', TrackSchema);
