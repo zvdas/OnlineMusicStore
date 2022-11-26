8903c4bb40d160d541565a3be7ca22494b766042
zvdas <rodriguezzude@gmail.com>
2022-11-26 20:25:01 +0530

    album ownership ready

--- backend_nodejs_v1/controllers/albums.js
+++ backend_nodejs_v1/controllers/albums.js
@@ -7,148 +7,211 @@ const ErrorResponse = require('../utils/errorResponse');
 // @desc    Get all albums
 // @route   GET /api/v1/albums
 // @access  Public
-exports.getAlbums = asyncHandler(async(req, res, next) => {
-    
-    res
-        .status(200)
-        .json(res.advancedResults);
+exports.getAlbums = asyncHandler(async (req, res, next) => {
+  res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get album by ID
 // @route   GET /api/v1/albums/:id
 // @access  Public
-exports.getAlbumById = asyncHandler(async(req, res, next) => {
-    const album = await AlbumModel.findById(req.params.id);
+exports.getAlbumById = asyncHandler(async (req, res, next) => {
+  const album = await AlbumModel.findById(req.params.id);
 
-    // error for correctly formatted id not present in database
-    if(!album) {
-        return next(new ErrorResponse(`Album with id '${req.params.id}' not found`, 404));
-    }
+  // error for correctly formatted id not present in database
+  if (!album) {
+    return next(
+      new ErrorResponse(`Album with id '${req.params.id}' not found`, 404)
+    );
+  }
 
-    res
-        .status(200)
-        .json({ 
-            success: true, 
-            data: album 
-        });    
+  res.status(200).json({
+    success: true,
+    data: album,
+  });
 });
 
 // @desc    Create new album
 // @route   POST /api/v1/albums
 // @access  Private
-exports.createAlbum = asyncHandler(async(req, res, next) => {
-    const albums = await AlbumModel.create(req.body);
+exports.createAlbum = asyncHandler(async (req, res, next) => {
+  // add user to the request body
+  req.body.user = req.user.id;
 
-    res
-        .status(201)
-        .json({ 
-            success: true, 
-            // data: albums,
-            msg: 'Album created successfully' 
-        });    
+  // check for a published album
+  const publishedAlbum = await AlbumModel.findOne({ user: req.user.id });
+
+  // a user who is not an admin can only add one album
+  if (req.user.role !== 'admin' && publishedAlbum) {
+    return next(
+      new ErrorResponse(
+        `User with id '${req.user.id}' has already published an album`,
+        400
+      )
+    );
+  }
+
+  const albums = await AlbumModel.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    // data: albums,
+    msg: 'Album created successfully',
+  });
 });
 
 // @desc    Update an album
 // @route   PUT /api/v1/albums/:id
 // @access  Private
-exports.updateAlbumById = asyncHandler(async(req, res, next) => {
-    const album = await AlbumModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+exports.updateAlbumById = asyncHandler(async (req, res, next) => {
+  let album = await AlbumModel.findById(req.params.id);
 
-    // error for correctly formatted id not present in database
-    if(!album) {
-        return next(new ErrorResponse(`Album with id '${req.params.id}' not found`, 404));
-    }
+  // error for correctly formatted id not present in database
+  if (!album) {
+    return next(
+      new ErrorResponse(`Album with id '${req.params.id}' not found`, 404)
+    );
+  }
 
-    res
-        .status(200)
-        .json({ 
-            success: true, 
-            msg: `Album with id '${req.params.id}' updated successfully`, 
-            data: album 
-            });    
+  // ensure user is the album owner or admin
+  if (album.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with ID '${req.user.id}' is not authorized to update this album`,
+        401
+      )
+    );
+  }
+
+  album = await AlbumModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    msg: `Album with id '${req.params.id}' updated successfully`,
+    data: album,
+  });
 });
 
 // @desc    Delete an album
 // @route   DELETE /api/v1/albums/:id
 // @access  Private
-exports.deleteAlbumById = asyncHandler(async(req, res, next) => {
-    const album = await AlbumModel.findById(req.params.id);
+exports.deleteAlbumById = asyncHandler(async (req, res, next) => {
+  const album = await AlbumModel.findById(req.params.id);
 
-    // error for correctly formatted id not present in database
-    if(!album) {
-        return next(new ErrorResponse(`Album with id '${req.params.id}' not found`, 404));
-    }
+  // error for correctly formatted id not present in database
+  if (!album) {
+    return next(
+      new ErrorResponse(`Album with id '${req.params.id}' not found`, 404)
+    );
+  }
 
-    album.remove();
+  // ensure user is the album owner or admin
+  if (album.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with ID '${req.user.id}' is not authorized to delete this album`,
+        401
+      )
+    );
+  }
 
-    res
-        .status(200)
-        .json({ 
-            success: true, 
-            msg: `Album with id '${req.params.id}' deleted successfully` 
-        });    
+  album.remove();
+
+  res.status(200).json({
+    success: true,
+    msg: `Album with id '${req.params.id}' deleted successfully`,
+  });
 });
 
 // @desc    Upload a cover photo for album
 // @route   PUT /api/v1/albums/:id/photo
 // @access  Private
-exports.albumPhotoUpload = asyncHandler(async(req, res, next) => {
-    const album = await AlbumModel.findById(req.params.id);
+exports.albumPhotoUpload = asyncHandler(async (req, res, next) => {
+  const album = await AlbumModel.findById(req.params.id);
 
-    // error for correctly formatted id not present in database
-    if(!album) {
-        return next(new ErrorResponse(`Album with id '${req.params.id}' not found`, 404));
+  // error for correctly formatted id not present in database
+  if (!album) {
+    return next(
+      new ErrorResponse(`Album with id '${req.params.id}' not found`, 404)
+    );
+  }
+
+  // ensure user is the album owner or admin
+  if (album.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User with ID '${req.user.id}' is not authorized to upload a cover photo for this album`,
+        401
+      )
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`Kindly upload a file`, 400));
+  }
+
+  const file = req.files.photo;
+
+  // ensure the image is a photo
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse(`Kindly upload an image file`, 400));
+  }
+
+  let max_photo_size =
+    process.env.MAX_PHOTO_UPLOAD.split('*')[0] *
+    process.env.MAX_PHOTO_UPLOAD.split('*')[1] *
+    process.env.MAX_PHOTO_UPLOAD.split('*')[2];
+  let mbs =
+    process.env.MAX_PHOTO_UPLOAD.split('*')[1] *
+    process.env.MAX_PHOTO_UPLOAD.split('*')[2];
+
+  // check file size (should be less than MAX_PHOTO_UPLOAD in config.env)
+  if (file.size > max_photo_size) {
+    return next(
+      new ErrorResponse(
+        `The uploaded image exceeds ${
+          max_photo_size / mbs
+        }MB. Kindly upload a smaller image.`,
+        400
+      )
+    );
+  }
+
+  // create custom filename
+  file.name = `cover_photo_${album.album_slug}${path.parse(file.name).ext}`;
+
+  // create folder with artist-album name if it doesn't exist
+  const folder_path = `${process.env.PHOTO_UPLOAD_PATH}/${album.artist_slug}-${album.album_slug}`;
+  if (!fs.existsSync(folder_path)) {
+    fs.mkdir(`${folder_path}`, async (err) => {
+      if (err) {
+        return next(
+          new ErrorResponse(`Problem with folder creation. ${err}`, 500)
+        );
+      }
+    });
+  }
+
+  // convert the file to buffer string and send to database
+  const image_base64 = file.data.toString('base64');
+
+  // upload the file
+  file.mv(`${folder_path}/${file.name}`, async (err) => {
+    if (err) {
+      return next(new ErrorResponse(`Problem with file upload. ${err}`, 500));
     }
 
-    if(!req.files) {
-        return next(new ErrorResponse(`Kindly upload a file`, 400));
-    }
+    await AlbumModel.findByIdAndUpdate(req.params.id, {
+      cover_photo: file.name,
+      cover_photo_data: image_base64,
+    });
 
-    const file = req.files.photo;
-
-    // ensure the image is a photo
-    if(!file.mimetype.startsWith('image')) {
-        return next(new ErrorResponse(`Kindly upload an image file`, 400));
-    }
-    
-    let max_photo_size = process.env.MAX_PHOTO_UPLOAD.split('*')[0] * process.env.MAX_PHOTO_UPLOAD.split('*')[1] * process.env.MAX_PHOTO_UPLOAD.split('*')[2];
-    let mbs = process.env.MAX_PHOTO_UPLOAD.split('*')[1] * process.env.MAX_PHOTO_UPLOAD.split('*')[2];
-
-    // check file size (should be less than MAX_PHOTO_UPLOAD in config.env)
-    if(file.size > max_photo_size) {
-        return next(new ErrorResponse(`The uploaded image exceeds ${max_photo_size/mbs}MB. Kindly upload a smaller image.`, 400));
-    }
-
-    // create custom filename
-    file.name = `cover_photo_${album.album_slug}${path.parse(file.name).ext}`;
-
-    // create folder with artist-album name if it doesn't exist
-    const folder_path = `${process.env.PHOTO_UPLOAD_PATH}/${album.artist_slug}-${album.album_slug}`;
-    if(!fs.existsSync(folder_path)){
-        fs.mkdir(`${folder_path}`, async err => {
-            if(err) {
-                return next(new ErrorResponse(`Problem with folder creation. ${err}`, 500));
-            }
-        });
-    }
-    
-    // convert the file to buffer string and send to database
-    const image_base64 = file.data.toString('base64');
-    
-    // upload the file
-    file.mv(`${folder_path}/${file.name}`, async err => {
-        if(err) {
-            return next(new ErrorResponse(`Problem with file upload. ${err}`, 500));
-        }
-
-        await AlbumModel.findByIdAndUpdate(req.params.id, { cover_photo: file.name, cover_photo_data: image_base64 })
-        
-        res
-            .status(200)
-            .json({ 
-                success: true, 
-                msg: `Cover photo uploaded to album with id '${req.params.id}' successfully`,
-                data: file.name
-            });    
-    })
+    res.status(200).json({
+      success: true,
+      msg: `Cover photo uploaded to album with id '${req.params.id}' successfully`,
+      data: file.name,
+    });
+  });
 });
--- backend_nodejs_v1/models/Album.js
+++ backend_nodejs_v1/models/Album.js
@@ -1,67 +1,75 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const AlbumSchema =  new mongoose.Schema({
+const AlbumSchema = new mongoose.Schema(
+  {
     album_name: {
-        type: String,
-        required: [true, 'Please add a name for the Album'],
-        unique: true,
-        trim: true,
-        maxlength: [50, 'Name cannot exceed 50 characters']
+      type: String,
+      required: [true, 'Please add a name for the Album'],
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name cannot exceed 50 characters'],
     },
     album_slug: String,
     // to slugify the url path (ie) "Admiral Bob" => "admiral-bob" for parsing url
     cover_photo: {
-        type: String,
-        default: 'no-photo.jpg'
+      type: String,
+      default: 'no-photo.jpg',
     },
     cover_photo_data: String,
     artist: {
-        type: String,
-        required: [true, 'Please add an artist\'s name'],
+      type: String,
+      required: [true, "Please add an artist's name"],
     },
     artist_slug: String,
     genre: String,
     year: Number,
     producer: String,
     averageRating: {
-        type: Number,
-        min: [1, 'Rating must be atleast 1'],
-        max: [5, 'Rating cannot be more than 5']
+      type: Number,
+      min: [1, 'Rating must be atleast 1'],
+      max: [5, 'Rating cannot be more than 5'],
     },
     description: {
-        type: String,
-        maxlength: [500, 'Name cannot exceed 500 characters']
+      type: String,
+      maxlength: [500, 'Name cannot exceed 500 characters'],
     },
     album_url: String,
     createdAt: {
-        type: Date,
-        default: Date.now()
-    }
-}, {
+      type: Date,
+      default: Date.now(),
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-});
+    toObject: { virtuals: true },
+  }
+);
 
 // create album slug from the name
-AlbumSchema.pre('save', function(next) {
-    this.album_slug = slugify(this.album_name, { lower: true });
-    this.artist_slug = slugify(this.artist, { lower: true });
-    next();
+AlbumSchema.pre('save', function (next) {
+  this.album_slug = slugify(this.album_name, { lower: true });
+  this.artist_slug = slugify(this.artist, { lower: true });
+  next();
 });
 
 // reverse populate with virtuals
 AlbumSchema.virtual('tracks', {
-    ref: 'Track',
-    localField: '_id',
-    foreignField: 'album',
-    justOne: false
+  ref: 'Track',
+  localField: '_id',
+  foreignField: 'album',
+  justOne: false,
 });
 
 // cascade delete tracks when an album is deleted
 AlbumSchema.pre('remove', async function (next) {
-    await this.model('Track').deleteMany({ album: this._id });
-    next();
+  await this.model('Track').deleteMany({ album: this._id });
+  next();
 });
 
 module.exports = mongoose.model('Album', AlbumSchema);
