be334f42b181f78a9319ebd3ed28bcaf7b041509
zvdas <rodriguezzude@gmail.com>
2022-12-08 21:08:23 +0530

    average rating logic added to review model

--- backend_nodejs_v1/models/Review.js
+++ backend_nodejs_v1/models/Review.js
@@ -34,4 +34,35 @@ const ReviewSchema = new mongoose.Schema({
 // prevent user from submitting more than one review per album
 ReviewSchema.index({ album: 1, user: 1 }, { unique: true });
 
+// static method to get the average rating and save
+ReviewSchema.statics.getAverageRating = async function (albumId) {
+  const obj = await this.aggregate([
+    {
+      $match: { album: albumId },
+    },
+    {
+      $group: { _id: '$album', averageRating: { $avg: '$rating' } },
+    },
+  ]);
+
+  // update rating in database and add as a field
+  try {
+    await this.model('Album').findByIdAndUpdate(albumId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// call getAverageRating after save
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.album);
+});
+
+// call getAverageRating before remove
+ReviewSchema.pre('remove', function () {
+  this.constructor.getAverageRating(this.album);
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
